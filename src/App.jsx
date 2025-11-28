6de19a54945d2f0342349c7fadc99130f4d6cb56
M Shafqat Rasool <mshafqat.developer@gmail.com>
2025-11-28 11:43:09 +0500

    first;

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Home from './pages/Home/Home'
+import { Route, Routes } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
